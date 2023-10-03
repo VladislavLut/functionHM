4a2ad864a665921fb59610c3cf1ad637550c6df5
VladislavLut <143324701+VladislavLut@users.noreply.github.com>
2023-10-03 19:12:58 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,7 @@
+def print_formatted_text(text, author, font, size, color):
+
+    formatted_text = f"{font}{size}{color}{text}\n-{author}"
+
+    print(formatted_text)
+
+print_formatted_text ("\"Don't compare yourself with anyone in this world… if you do so, you are insulting yourself...\" ","Bill Gates", "Colibri", 24, "gray")
