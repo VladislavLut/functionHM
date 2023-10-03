58823c1629d1d23c5c2c6fa4b887bbd1e04f4167
VladislavLut <143324701+VladislavLut@users.noreply.github.com>
2023-10-03 20:38:30 +0300

    ex-7 was done

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-def count_digits(number):
+def is_pollindrome(number):
 
     number_string = str(number)
 
-    return len(number_string)
+    first_half = number_string[:len(number_string) // 2]
+    second_half = number_string[len(number_string) // 2:][::-1]
 
-print(count_digits(3456))
+    return first_half == second_half
+print(is_pollindrome(123321))
